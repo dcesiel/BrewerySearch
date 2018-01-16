6607e944b3627f54f826779e24205f6287cb6e5c
David Cesiel <dcesiel@gmail.com>
2018-01-16 11:25:49 -0600

    Adding form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrewerySearchComponent } from './brewery-search/brewery-search.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrewerySearchComponent
+    BrewerySearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/brewery-search/brewery-search.component.ts
+++ src/app/brewery-search/brewery-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-brewery-search',
@@ -8,14 +9,24 @@ import { Component, OnInit } from '@angular/core';
 
 export class BrewerySearchComponent implements OnInit {
 
+  searchForm: FormGroup;
+
   private apiEndpoint = 'http://api.brewerydb.com/v2/locations?key=7a92accaaec73fe3eb08ee2b22e12be4&postalCode=49053';
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {}
 
   onSearch() {
-    console.log('Search clicked');
+    console.log(this.searchForm.get('search').value);
+  }
+
+  createForm() {
+    this.searchForm = this.formBuilder.group({
+      search: ''
+    });
   }
 
 }
