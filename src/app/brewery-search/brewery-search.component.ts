12536d52e7d58a95930083720427b7b15fda3bf5
David Cesiel <dcesiel@gmail.com>
2018-01-16 22:52:31 -0600

    Adding request with console log

--- src/app/brewery-search/brewery-search.component.ts
+++ src/app/brewery-search/brewery-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-brewery-search',
@@ -11,16 +12,19 @@ export class BrewerySearchComponent implements OnInit {
 
   searchForm: FormGroup;
 
-  private apiEndpoint = 'http://api.brewerydb.com/v2/locations?key=7a92accaaec73fe3eb08ee2b22e12be4&postalCode=49053';
+  // Another API key just in case: 5e649ec07aa7f2b8b9f5902dbe763b24
+  private apiEndpoint = '/v2/locations?key=7a92accaaec73fe3eb08ee2b22e12be4&postalCode=';
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
     this.createForm();
   }
 
   ngOnInit() {}
 
   onSearch() {
-    console.log(this.searchForm.get('search').value);
+    this.http.get(this.apiEndpoint + this.searchForm.get('search').value).subscribe(response => {
+      console.log(response);
+    });
   }
 
   createForm() {
