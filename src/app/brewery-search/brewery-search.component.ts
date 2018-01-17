62a9f2ab022c77b0f4dd7316bb93bab5a6bf1822
David Cesiel <dcesiel@gmail.com>
2018-01-16 22:44:16 -0600

    Adding an additional API key

--- src/app/brewery-search/brewery-search.component.ts
+++ src/app/brewery-search/brewery-search.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class BrewerySearchComponent implements OnInit {
 
+  // Another API key just in case: 5e649ec07aa7f2b8b9f5902dbe763b24
   private apiEndpoint = 'http://api.brewerydb.com/v2/locations?key=7a92accaaec73fe3eb08ee2b22e12be4&postalCode=49053';
 
   constructor() { }
