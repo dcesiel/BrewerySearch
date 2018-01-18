2b0b359d461bd67ad9dfae8fcaef772b80e6f94b
David Cesiel <dcesiel@gmail.com>
2018-01-18 11:17:56 -0600

    Modifying util

--- src/app/brewery-search/brewery-search-util.ts
+++ src/app/brewery-search/brewery-search-util.ts
@@ -1,19 +1,21 @@
-
-export class BrewerySearchUtil {
-
-  // static setBreweryArray(response: any) {
-  //   const breweryArray = new Array<Brewery>();
-  //   if (response.data) {
-  //     response.data.forEach(breweryInfo => {
-  //       const brewery = new Brewery();
-  //       brewery.name = breweryInfo.brewery.name;
-  //       brewery.streetAddress = breweryInfo.streetAddress;
-  //       brewery.website = breweryInfo.brewery.website;
-  //       brewery.logoUrl = (breweryInfo.brewery.images) ? breweryInfo.brewery.images.icon : '';
-  //       breweryArray.push(brewery);
-  //     });
-  //   }
-  //   this.breweryArray = breweryArray;
-  // }
-
-}
+// import {Brewery} from './brewery';
+//
+//
+// export class BrewerySearchUtil {
+//
+//   static setBreweryArray(response: any) {
+//     const breweryArray = new Array<Brewery>();
+//     if (response.data) {
+//       response.data.forEach(breweryInfo => {
+//         const brewery = new Brewery();
+//         brewery.name = breweryInfo.brewery.name;
+//         brewery.streetAddress = breweryInfo.streetAddress;
+//         brewery.website = breweryInfo.brewery.website;
+//         brewery.logoUrl = (breweryInfo.brewery.images) ? breweryInfo.brewery.images.icon : '';
+//         breweryArray.push(brewery);
+//       });
+//     }
+//     return breweryArray;
+//   }
+//
+// }
